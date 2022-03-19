70b2d1fcd21de15e9688858bb23a124975a10140
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-19 12:07:27 +0200

    fix test setup and file pattern parsing

--- main.py
+++ main.py
@@ -5,27 +5,54 @@ class Main:
     ITERATIONS = 0
     X = 0
     Y = 0
+    PATTERN = [[]]
 
     def __init__(self, file, iterations):
         self.FILE = file
         self.ITERATIONS = iterations
+        self.X = 0
+        self.Y = 0
+        self.PATTERN = [[]]
         
         with open(self.FILE) as file:
             lines = [line.strip() for line in file]
 
         for line in lines:
+            # Comment
             if line[0] == "#":
                 continue
+            # Coords, rule
             if line[0] == "x":
                 coords = line.split(",")
                 self.X = int(coords[0].split("=")[-1].strip())
                 self.Y = int(coords[1].split("=")[-1].strip())
+                continue
+            # Pattern
+            i = 0
+            reps = 1
+            for char in line:
+                if char == "!":
+                    continue
+                if char == "$":
+                    self.PATTERN.append([])
+                    i += 1
+                    continue
                 
+                if char.isnumeric():
+                    reps = int(char)
+                    continue
+                for j in range(reps):
+                    self.PATTERN[i].append(char)
+                reps = 1
+
     def get_width(self):
         return self.X
 
     def get_height(self):
         return self.Y
+
+    def get_pattern(self):
+        return self.PATTERN
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
--- test/main_test.py
+++ test/main_test.py
@@ -3,12 +3,17 @@ import main
 
 class TestStringMethods(unittest.TestCase):
 
+    app = None
+    def setUp(self):
+        self.app = main.Main("glider.rle", 1)
+
     def test_main_takes_file_and_iterations(self):
-        app = main.Main("glider.rle", 1)
-        self.assertEqual(app.FILE, "glider.rle")
-        self.assertEqual(app.ITERATIONS, 1)
+        self.assertEqual(self.app.FILE, "glider.rle")
+        self.assertEqual(self.app.ITERATIONS, 1)
 
     def test_file_parses_x_and_y(self):
-        app = main.Main("./glider.rle", 1)
-        self.assertEqual(app.get_width(), 3)
-        self.assertEqual(app.get_height(), 3)
+        self.assertEqual(self.app.get_width(), 3)
+        self.assertEqual(self.app.get_height(), 3)
+    
+    def test_file_pattern_parsed(self):
+        self.assertEqual(self.app.get_pattern(), [["b", "o", "b"], ["b", "b", "o"], ["o", "o", "o"]])
