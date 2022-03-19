953afae4553a884dca4a472e0515bddb3f241e3c
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-19 13:02:14 +0200

    test blinker x and y

--- test/main_test.py
+++ test/main_test.py
@@ -26,9 +26,9 @@ class TestFileParsingBlinker(unittest.TestCase):
         self.assertEqual(self.app.FILE, "blinker.rle")
         self.assertEqual(self.app.ITERATIONS, 1)
 
-    # def test_file_parses_x_and_y(self):
-    #     self.assertEqual(self.app.get_width(), 3)
-    #     self.assertEqual(self.app.get_height(), 3)
+    def test_file_parses_x_and_y(self):
+        self.assertEqual(self.app.get_width(), 3)
+        self.assertEqual(self.app.get_height(), 1)
     
     # def test_file_pattern_parsed(self):
     #     self.assertEqual(self.app.get_pattern(), [["b", "o", "b"], ["b", "b", "o"], ["o", "o", "o"]])
