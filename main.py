551c469b671feeb2c13fb4b0adad774f2d35d736
jpasikainen <asikainen.juhapekka@gmail.com>
2022-03-19 11:37:00 +0200

    parse x and y

--- main.py
+++ main.py
@@ -3,14 +3,33 @@ import sys
 class Main:
     FILE = ""
     ITERATIONS = 0
+    X = 0
+    Y = 0
 
     def __init__(self, file, iterations):
         self.FILE = file
         self.ITERATIONS = iterations
+        
+        with open(self.FILE) as file:
+            lines = [line.strip() for line in file]
+
+        for line in lines:
+            if line[0] == "#":
+                continue
+            if line[0] == "x":
+                coords = line.split(",")
+                self.X = int(coords[0].split("=")[-1].strip())
+                self.Y = int(coords[1].split("=")[-1].strip())
+                
+    def get_width(self):
+        return self.X
+
+    def get_height(self):
+        return self.Y
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        file = sys.argv.pop()
         iterations = sys.argv.pop()
-        Main(file, iterations)
+        file = sys.argv.pop()
+        Main(file, iterations).get_width()
 
--- test/main_test.py
+++ test/main_test.py
@@ -4,6 +4,11 @@ import main
 class TestStringMethods(unittest.TestCase):
 
     def test_main_takes_file_and_iterations(self):
-        app = main.Main("file", 1)
-        self.assertEqual(app.FILE, "file")
+        app = main.Main("glider.rle", 1)
+        self.assertEqual(app.FILE, "glider.rle")
         self.assertEqual(app.ITERATIONS, 1)
+
+    def test_file_parses_x_and_y(self):
+        app = main.Main("./glider.rle", 1)
+        self.assertEqual(app.get_width(), 3)
+        self.assertEqual(app.get_height(), 3)
